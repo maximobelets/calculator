30618d2bf534c4afa065b73766c4a3578b2e0a67
Maxim Obelets <maximobelets@gmail.com>
2019-05-24 00:41:04 +0500

    Fixed handler to get data

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+'use strict'
+
 const data = document.querySelector('.get-data');
 const elements = document.querySelectorAll('.field-item');
 
-for(i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', function () {
+
+const getElement = element => {
+    element.addEventListener('click', function() {
         data.value += this.innerHTML;
-        if (this.innerHTML === 'AC') {
-            data.value = '';
-        }
     })
 };
+
+elements.forEach(getElement);
+
+
+
+
 
