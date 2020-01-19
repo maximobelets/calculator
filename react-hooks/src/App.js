8f86bb82df9cbf480807edebb43c5cca97189208
Maxim Obelets <maximobelets@gmail.com>
2020-01-19 19:47:54 +0500

    fixed app component

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -9,9 +9,31 @@ const App = () => {
     <div>
       <h4>Calculator React-Hooks</h4>
       <Input />
-      <CalcItem name={1}/>
-      <CalcItem name={2}/>
-      <CalcItem name={3}/>
+      <div>
+        <CalcItem name={7}/>
+        <CalcItem name={8}/>
+        <CalcItem name={9}/>
+        <CalcItem name={'+'}/>
+      </div>
+      <div>
+        <CalcItem name={4}/>
+        <CalcItem name={5}/>
+        <CalcItem name={6}/>
+        <CalcItem name={'-'}/>
+      </div>
+      <div>
+        <CalcItem name={1}/>
+        <CalcItem name={2}/>
+        <CalcItem name={3}/>
+        <CalcItem name={'%'}/>
+      </div>
+      <div>
+        <CalcItem name={0}/>
+        <CalcItem name={'.'}/>
+        <CalcItem name={'AC'}/>
+        <CalcItem name={'X'}/>
+      </div>
+      <CalcItem name={'='}/>
     </div>
   )
 };
